de72625a7a10ff28c39479dbc3b352fef6bb02d4
wudada <2567199685@qq.com>
2022-03-14 13:59:59 +0800

    通过异步操作和动态导入的方式实现了懒加载(import())

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import x from "./x.js"
 import jpg from "./assets/1.jpg"
+
 const div = document.getElementById("app")
 div.innerHTML = `<img src = "${jpg}">`
+
+const button = document.createElement("button")
+button.innerText = "懒加载"
+button.onclick = () => {
+  const promise = import("./lazy")  // import() 是动态导入，会返回一个 promise
+  promise.then((Module) => {
+    console.log(Module.default())
+  })  // 返回的是一个模块
+}
+div.appendChild(button)
